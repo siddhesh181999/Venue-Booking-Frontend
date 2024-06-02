6fa0261c2ed1c6bd3e18d02992ae55ee3f32ae7f
siddhesh181999 <59130104+siddhesh181999@users.noreply.github.com>
2024-06-02 19:38:35 +0530

    async await

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,8 @@ function Home() {
   const[countries, setCountries] = useState([]);
   const[selectedCountry, setSelectedCountry] = useState("");
   useEffect(()=>{
-    fetch('https://countriesnow.space/api/v0.1/countries')
+    const fetchCountries = async() =>{
+      await fetch('https://countriesnow.space/api/v0.1/countries')
       .then(response => {
         if (!response.ok) {
           throw new Error('Network response was not ok');
@@ -16,8 +17,11 @@ function Home() {
         return response.json();
       })
       .then(data => {
-        setCountries(data.data);
+        let response = data.data;
+        setCountries(response);
       })
+    }
+    fetchCountries();
   },[])
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -26,11 +30,11 @@ function Home() {
         <Grid xs={2.5}>
         </Grid>
         <Grid xs={3}>
-          {countries.length > 0 ? (<AutocompleteForCountries countries={countries} setCountries={setSelectedCountry} />) : ""}
+          <AutocompleteForCountries countries={countries.length > 0 ? countries : []} setCountries={setSelectedCountry} />
         </Grid>
         <Grid xs={1} />
         <Grid xs={3}>
-          {countries.length > 0 ? (<AutocompleteForCities cities={selectedCountry ? countries.find((x)=> x.country === selectedCountry).cities : []} />) : ""}
+          <AutocompleteForCities cities={selectedCountry && countries.length > 0 ? countries.find((x)=> x.country === selectedCountry).cities : []} />
         </Grid>
         <Grid xs={2.5}>
         </Grid>
